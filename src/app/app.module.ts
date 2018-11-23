015204fda972abed1feb20b579ab6ce57d690d9f
Ram Lanka <Ramlanka@outlook.com>
2018-11-22 21:01:40 -0500

    Created new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,54 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule, MatCheckboxModule,
+  MatSelectModule, MatFormFieldModule,
+  MatInputModule, MatToolbarModule
+} from '@angular/material';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {HomeComponent} from './home/home.component';
+import {UpdatepasswordComponent} from './updatepassword/updatepassword.component';
+import {PrivacyComponent} from './privacy/privacy.component';
+import {TermsofuseComponent} from './termsofuse/termsofuse.component';
+
+const appRoutes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '**', component: HomeComponent}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    UpdatepasswordComponent,
+    PrivacyComponent,
+    TermsofuseComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} // <-- debugging purposes only
+    ),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule, MatFormFieldModule,
+    MatToolbarModule, MatSelectModule, MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
